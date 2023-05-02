06507d97e6550dc6b2ed03b05c7dc3815c93358c
lukassuprer <lukaswedry@seznam.cz>
2023-05-02 20:06:26 +0200

    Auto stash before rebase of "origin/main"

--- website/dealership/src/components/app.jsx
+++ website/dealership/src/components/app.jsx
@@ -1,10 +1,10 @@
 import { CssVarsProvider } from '@mui/joy/styles';
 import {Route, Routes} from 'react-router-dom';
 import Home from './home.jsx';
+import CarForm from './car-form.jsx';
+import NotFound from './not-found.jsx';
 import React from 'react';
-import {extendTheme} from "@mui/joy";
 import '@fontsource/public-sans/400.css'
-
 import '@fontsource/public-sans/300.css';
 import Developers from "./developers";
 
@@ -16,6 +16,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/offers" element={<Home />} />
           <Route path="/fordevelopers" element={<Developers/>} />
+        <Route path="/buy/:id" element={<CarForm />} />
+        <Route path='*' element={<NotFound />}/>
       </Routes>
     </CssVarsProvider>
   );
--- website/dealership/src/components/car-form.jsx
+++ website/dealership/src/components/car-form.jsx
@@ -1,7 +1,11 @@
 import MainLayout from "./main-layout.jsx";
 import React from 'react';
+import {useParams} from "react-router-dom";
 
 export default function CarForm() {
+	const {id} = useParams();
+	console.log(id);
+
 	return (
 		<MainLayout>
 			<h1>Car form</h1>
