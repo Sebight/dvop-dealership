17f3805b07383f19c03d9284f8a7fccfe960e635
Sebight <seb1rblx@gmail.com>
2023-05-06 13:22:07 +0200

    Implement SMTP

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,49 +1,57 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+let password = process.env.GMAIL_PASSWORD;
+let user = process.env.GMAIL_USER;
+
+console.log(password);
+console.log(user);
 let transporter = nodemailer.createTransport({
-    //? pool: true,
-    host: "smtp.example.com",
-    port: 587,
-    secure: false, // upgrade later with STARTTLS
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
-        user: "username",
-        pass: "password",
+        user: user,
+        pass: password,
     },
 });
 
 function verifyConnection() {
-    let result = false;
-    transporter.verify(function (error, success) {
-        if (error) {
-            console.log(error);
-            result = false;
-        } else {
-            console.log("Server is ready to take our messages");
-            result = true;
-        }
+    return new Promise((resolve, reject) => {
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log("Verification failed!");
+                console.log(error);
+                reject(false);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(true);
+            }
+        });
     });
-    return result;
 }
 
-function sendEmail(to, subject, text, html) {
-    if (verifyConnection() === false) {
+async function sendEmail(to, subject, text, html) {
+    try {
+        await verifyConnection();
+    } catch (error) {
         return;
     }
 
     let mailOptions = {
-        from: 'sender@sender.com',
+        from: "DVOP Dealership",
         to: to,
         subject: subject,
         text: text,
-        html: html
-    }
+        html: html,
+    };
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             return console.log(error);
         }
-        console.log('Message %s sent: %s', info.messageId, info.response);
+        console.log("Message %s sent: %s", info.messageId, info.response);
     });
 }
 
 module.exports = {
-    sendEmail
-}
+    sendEmail,
+};
