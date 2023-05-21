04ea540cd9261c0765130b5d3e87b23a220183d6
Sebight <59232422+Sebight@users.noreply.github.com>
2023-05-21 20:12:23 +0200

    hide sold cars

--- website/dealership/src/components/home.jsx
+++ website/dealership/src/components/home.jsx
@@ -7,28 +7,30 @@ import {getCars} from "../tools/fetcher";
 
 
 export default function Home() {
-	const [cars, setCars] = useState([]);
+    const [cars, setCars] = useState([]);
 
-	useEffect(() => {
-		getCars(setCars);
-	}, [])
+    useEffect(() => {
+        getCars(setCars);
+    }, [])
 
-	return (
-		<MainLayout>
-			<Box
-				display="flex"
-				justifyContent="center"
-				alignItems="center"
-				minHeight="100vh"
-				flexDirection="column"
-				gap="1rem"
-			>
-				{cars.map((car) => {
-					return (
-						<CardButton car={car} key={car.id}/>
-					)
-				})}
-			</Box>
-		</MainLayout>
-	);
+    return (
+        <MainLayout>
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                minHeight="100vh"
+                flexDirection="column"
+                gap="1rem"
+            >
+                {cars.map((car) => {
+                    if (car.sold === false) {
+                        return (
+                            <CardButton car={car} key={car.id}/>
+                        )
+                    }
+                })}
+            </Box>
+        </MainLayout>
+    );
 }
